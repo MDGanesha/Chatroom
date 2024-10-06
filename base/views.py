6caf6dc563380e4af4a63a2f06f24fcf31002d08
MDGanesha <ganeshamd24@gmail.com>
2024-10-06 23:52:55 +0530

    day2

--- base/views.py
+++ base/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .models import Room, Topic
+from .models import Room, Topic, Message
 from django.db.models import Q
 from django.contrib.auth.decorators import login_required
 from .froms import RoomForm
@@ -59,13 +59,24 @@ def home(request):
                                 Q(description__icontains = q))
     topics = Topic.objects.all()
     room_count = rooms.count()
-    context = {'rooms':rooms,'topics':topics,'room_count':room_count}
+    room_messages = Message.objects.all()
+    context = {'rooms':rooms,'topics':topics,'room_count':room_count,'room_messages':room_messages}
     return render(request,'base/home.html',context)
 
 
 def room(request,pk):
     room = Room.objects.get(id = pk)
-    context = {"room":room}
+    room_messages = room.message_set.all().order_by('-created')
+    participants = room.participants.all()
+    if(request.method=="POST"):
+        message = Message.objects.create(
+            user=request.user,
+            room=room,
+            body = request.POST.get("body")
+        )
+        room.participants.add(request.user)
+        return redirect('room',pk=room.id)
+    context = {"room":room,"room_messages":room_messages,'participants':participants}
     return render(request, 'base/room.html',context)
 
 @login_required(login_url='/login')
@@ -103,3 +114,12 @@ def deleteRoom(request,pk):
         return redirect('home')
     context = {'room':room}
     return render(request,'base/delete.html',context)
+@login_required(login_url='/login')
+def deleteMessage(request,pk):
+    message = Message.objects.get(id=pk)
+    if(request.user != message.user):
+        return HttpResponse("You are not allowed to delete")
+    if(request.method=="POST"):
+        message.delete()
+        return redirect("home")
+    return render(request,'base/delete.html',{'obj':message})
